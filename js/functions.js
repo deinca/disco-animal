64f6fd37d4e1d56873eee0ae56ddd29a0c3b76c0
Loquino <zamoradhva@gmail.com>
2020-02-21 01:43:43 +0100

    add mod

--- js/functions.js
+++ js/functions.js
@@ -4,22 +4,41 @@ let audioControls = document.getElementById('audioControls');
 
 
 play.addEventListener('click', function(){
+
+    // This is the class that activates the elements if the play button is clicked
     document.body.classList.add("spelen");
+
+    // Play the music
     document.getElementById('soundTrack').play();
 });
 
 pause.addEventListener('click', function(){
+
+    // This removes the class that activates everything
     document.body.classList.remove("spelen");
+    
+    // Pause the music
     document.getElementById('soundTrack').pause();
 });
 
-document.querySelector(".geelLicht").addEventListener("click", function(){
-    document.body.classList.toggle("geelLicht");
+document.querySelector(".yellowLight").addEventListener("click", function(event){
+    // This manipualates the lights
+    document.body.classList.toggle("yellowLight");
+});
+
+document.querySelector('.blueLight').addEventListener("click", function(event){
+    // This manipualates the lights
+    document.body.classList.toggle("blueLight");
+});
+
+document.querySelector('.orangeLight').addEventListener("click", function(event){
+    // This manipualates the lights
+    document.body.classList.toggle("orangeLight");
 });
 
 audioControls.addEventListener('click', 
     function(event){
-        controlDis = document.getElementById('soundTrack').classList.toggle('apare');
+        document.body.classList.toggle("show");
     }
 );
 
